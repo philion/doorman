0fc28517001afad2365caa836e784cc8a3b15378
Paul Philion <philiob@acmerocket.com>
2016-09-15 11:31:08 -0700

    Initial code checkin. This resolves #1.

---
+++ src/main/java/com/acmerocket/DoormanApplication.java
@@ -0,0 +1,29 @@
+package com.acmerocket;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DoormanApplication extends Application<DoormanConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new DoormanApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "doorman";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<DoormanConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final DoormanConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/acmerocket/DoormanConfiguration.java
@@ -0,0 +1,10 @@
+package com.acmerocket;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class DoormanConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
